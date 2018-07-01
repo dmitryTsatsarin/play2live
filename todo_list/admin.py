93c297b026e8269d48d054b073f04e53901faeb8
dmitry <dmitry.tsatsarin@gmail.com>
2018-07-01 22:52:49 +0300

    Added django admin for Todo model

--- todo_list/admin.py
+++ todo_list/admin.py
@@ -1,1 +1,12 @@
 # Register your models here.
+from django.contrib import admin
+from django.contrib.admin import ModelAdmin
+
+from todo_list.models import Todo
+
+
+class TodoList(ModelAdmin):
+    pass
+
+
+admin.site.register(Todo, TodoList)
